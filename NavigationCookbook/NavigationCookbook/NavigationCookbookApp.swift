88c5040659603461670c6859903452042d9f02b0
YangJoonHyeok <zzing1031@gmail.com>
2024-06-06 22:18:23 +0900

    chore: 불필요한 파일 삭제

--- NavigationCookbook/NavigationCookbook/NavigationCookbookApp.swift
+++ NavigationCookbook/NavigationCookbook/NavigationCookbookApp.swift
@@ -1,26 +1,18 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-The main app, which creates a scene that contains a window group, displaying
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ The main app, which creates a scene that contains a window group, displaying
  the root content view.
-*/
+ */
 
 import SwiftUI
 
 @main
 struct NavigationCookbookApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                #if os(macOS)
-                .frame(minWidth: 800, minHeight: 600)
-                #endif
-        }
-        #if os(macOS)
-        .commands {
-            SidebarCommands()
-        }
-        #endif
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
