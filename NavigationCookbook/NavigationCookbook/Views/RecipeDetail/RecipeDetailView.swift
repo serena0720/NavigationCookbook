b7d64847f0ebcad344bc3034d4baeffb0d0aee06
YangJoonHyeok <zzing1031@gmail.com>
2024-06-09 10:53:54 +0900

    wip

--- NavigationCookbook/NavigationCookbook/Views/RecipeDetail/RecipeDetailView.swift
+++ NavigationCookbook/NavigationCookbook/Views/RecipeDetail/RecipeDetailView.swift
@@ -38,47 +38,40 @@ struct RecipeDetailView: View {
   }
   
   private var narrowDetails: some View {
-    let alignment: HorizontalAlignment = .center
-    
-    return VStack(alignment: alignment) {
-      image
-      ingredients
+		VStack(alignment: .center) {
+			image
+			ingredients
 			relatedRecipes
-    }
+		}
   }
   
-  @ViewBuilder
   private var image: some View {
-    RecipePhoto(url: store.image)
-        .frame(width: 300, height: 300)
-    
-  }
+		RecipePhoto(url: store.image)
+			.frame(width: 300, height: 300)
+	}
   
   private var ingredients: some View {
-    let padding = EdgeInsets(top: 16, leading: 0, bottom: 8, trailing: 0)
-    
-    return VStack(alignment: .leading) {
-      Text("Ingredients")
-        .font(.headline)
-        .padding(padding)
-      VStack(alignment: .leading) {
+		VStack(alignment: .leading) {
+			Text("Ingredients")
+				.font(.headline)
+				.padding(EdgeInsets(top: 16, leading: 0, bottom: 8, trailing: 0))
+			VStack(alignment: .leading) {
 				ForEach(store.recipe.ingredients) { ingredient in
-            Text(ingredient.description)
-          }
-      }
-    }
-    .frame(minWidth: 300, alignment: .leading)
+					Text(ingredient.description)
+				}
+			}
+		}
+		.frame(minWidth: 300, alignment: .leading)
   }
   
   @ViewBuilder
   var relatedRecipes: some View {
-    let padding = EdgeInsets(top: 16, leading: 0, bottom: 8, trailing: 0)
-    
 		if !store.recipe.related.isEmpty {
       VStack(alignment: .leading) {
         Text("Related Recipes")
           .font(.headline)
-          .padding(padding)
+          .padding(EdgeInsets(top: 16, leading: 0, bottom: 8, trailing: 0))
+				
         LazyVGrid(columns: columns, alignment: .leading) {
 					let relatedRecipes = getRelatedRecipes(with: store.recipes, for: store.recipe)
 					ForEach(relatedRecipes) { relatedRecipe in
@@ -95,8 +88,8 @@ struct RecipeDetailView: View {
 	
 	func getRelatedRecipes(with recipes: [Recipe], for recipe: Recipe) -> [Recipe] {
 		recipes
-				.filter { recipe.related.contains($0.id) }
-				.sorted { $0.name < $1.name }
+			.filter { recipe.related.contains($0.id) }
+			.sorted { $0.name < $1.name }
 	}
   
   private var columns: [GridItem] {
